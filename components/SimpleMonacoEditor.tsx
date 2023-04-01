0a1420a4623b577318b1d4527d60e3aeca5857e5
douglasliralima <douglasliralima@gmail.com>
2023-04-01 17:11:32 -0300

    constant folder

--- components/SimpleMonacoEditor.tsx
+++ components/SimpleMonacoEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Editor from "@monaco-editor/react";
-import draculaTheme from "./DraculaTheme";
+import draculaTheme from "../constants/DraculaTheme";
 
 interface Props {
     language?: string;
